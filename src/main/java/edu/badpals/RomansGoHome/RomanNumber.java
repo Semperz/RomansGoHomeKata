5ee142e6d6a54b8f7c7c4519a352f718915885f7
Sergio Prieto García <serprigar@gmail.com>
2024-04-23 12:50:12 +0200

    feat(RomanNumber.java): complete userStory

--- src/main/java/edu/badpals/RomansGoHome/RomanNumber.java
+++ src/main/java/edu/badpals/RomansGoHome/RomanNumber.java
@@ -1,7 +1,9 @@
 package edu.badpals.RomansGoHome;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class RomanNumber {
     private String romanNumber;
@@ -16,19 +18,31 @@ public class RomanNumber {
     }
 
     public int toDecimal() {
-            // TODO: Implement this
+        // TODO: Implement this
         Matcher matcher = romanRegex.matcher(getRomanNumber());
         if (!matcher.find()) {
             return 0;
         }
-        System.out.println(matcher.groupCount());
+        int total = 0;
         for (int i = 0; i < matcher.groupCount(); ++i) {
             String currentGroup = matcher.group(i);
             System.out.println(currentGroup);
-            System.out.println(RomanNumbers.valueOf(currentGroup).getDecimal());
+            int[] parsedData = currentGroup.chars().mapToObj(c -> (char) c).mapToInt(numeral -> RomanNumbers.valueOf(Character.toString(numeral)).getDecimal()).toArray();
+            for (int j = 0; j < parsedData.length; ++j){
+                if (j + 1 >= parsedData.length) {
+                    total += parsedData[j];
+                    break;
+                }
+                if (parsedData[j] >= parsedData[j+1]){
+                    total += parsedData[j];
+                }else{
+                    total += parsedData[j+1] - parsedData[j++];
+                }
+            }
+            System.out.println(Arrays.toString(parsedData));
         }
 
-        return 0;
+        return total;
     }
 
     @Override
